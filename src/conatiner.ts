0cc599d70cf39d62ff5ad4a66e07eee0106f9c8b
psegarra <pausegarra.b@gmail.com>
2024-01-09 15:40:45 +0100

    🐛 optional options

--- src/conatiner.ts
+++ src/conatiner.ts
@@ -1,12 +1,12 @@
 type RegisterOptions = {
-  loadDependencies: boolean;
-  isInstance: boolean;
+  loadDependencies?: boolean;
+  isInstance?: boolean;
 }
 
 export class Container {
   static dependencies = new Map<string, any>();
 
-  static register<T>(token: string, constructor: any, { loadDependencies = false, isInstance = false }: RegisterOptions) {
+  static register<T>(token: string, constructor: any, { loadDependencies = false, isInstance = false }: RegisterOptions = {}) {
     let instance;
     if (isInstance) {
       instance = constructor;
